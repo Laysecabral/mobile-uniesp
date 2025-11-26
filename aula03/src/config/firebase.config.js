044749b4dd9226cb578b1547c3d627f1432108ee
Laysecabral <laysebcabral@gmail.com>
2025-11-26 19:20:55 -0300

    Adiciona pasta aula03 com exercícios/aulas

---
+++ aula03/src/config/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcMdrshYYSmZz1JgxoXLgJC8gjgNzLBKI",
+  authDomain: "react-native-uniesp-56d38.firebaseapp.com",
+  projectId: "react-native-uniesp-56d38",
+  storageBucket: "react-native-uniesp-56d38.firebasestorage.app",
+  messagingSenderId: "804147365502",
+  appId: "1:804147365502:web:7e23852b0b441fb1713a2f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
